b460b021427cfbb68d3d9c8115f3b8f082386b9b
cdyall <cdyall1@gmail.com>
2019-05-18 21:57:06 -0400

    round3

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ var fs = require("fs");
 
 //variables that take in the command line arguments
 var liriCom = process.argv[2];
-var input = process.argv[3];
+var input = process.argv.slice(3).join(" ");
 
 //divider to seperate logs in log.txt file.
 var divider =
@@ -40,5 +40,105 @@ function command(liriCom, input){
 }
 
 
+// function for "concert-this" command. 
+function conInfo(input) {
+  // query url
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +input +"/events?app_id=codingbootcamp";
+
+  // Print the query url.
+  console.log(queryUrl);
+
+  // Performing get request
+  axios.get(queryUrl).then(function(response) {
+    var bandInfo = response.data[0];
+
+     // properties end up being the string containing the show data we will print to the console
+    var eveInfo = [
+      "Band:" + bandInfo.lineup,
+      "Venue:" + bandInfo.venue.name,
+      "City:" + bandInfo.venue.city,
+      "Date:" + bandInfo.datetime
+    ].join("\n\n");
+
+     // Append properties and the divider to log.txt, print showData to the console
+    fs.appendFile("log.txt", eveInfo + divider, function(err) {
+      if (err) throw err;
+      console.log(eveInfo);
+    });
+  });
+}
+
+// function for "spotify-this-song" command.
+function spotInfo(songName) {
+  var spotify = new Spotify(keys.spotify);
+
+  //If no song is provided, use "The Sign" 
+      if (!songName) {
+          songName = "The Sign";
+      };        
+
+      console.log(songName);
+
+      //Callback to spotify to search for song name
+      spotify.search({ type: 'track', query: songName}, function(err, data) {
+          if (err) {
+              return console.log('Error occurred: ' + err);
+          } 
+
+           // properties end up being the string containing the show data we will print to the console
+          var songInfo = [
+            "Artist: " + data.tracks.items[0].artists[0].name,
+            "Song name: " + data.tracks.items[0].name,
+            "Album Name: " + data.tracks.items[0].album.name,
+            "Preview Link: " + data.tracks.items[0].preview_url
+           ] .join("\n\n");
+               
+          //Appends text to log.txt file
+          fs.appendFile('log.txt', songInfo + divider, function (err) {
+              if (err) throw err;
+              console.log(songInfo);
+            });    
+      });
+};
 
 
+function movInfo(movieName) {
+
+ //If no movie name is provided, use Mr.Nobody as default
+ if (!movieName) {
+  movieName = "mr nobody";
+}
+
+  // query url
+  var queryUrl = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy";
+
+  // Print the query url.
+  console.log(queryUrl);
+
+  // Performing get request
+  axios.get(queryUrl).then(function(response) {
+    var movieResult = response.data;
+
+     // properties end up being the string containing the show data we will print to the console
+    var listing = [
+      "Title:" +movieResult.Title,
+      "Year:" +movieResult.Year,
+      "IMDB Rating:" +movieResult.Ratings[0],
+      "Rotten Tomatoes Rating:" +movieResult.Ratings[1],
+      "Country:" +movieResult.Country,
+      "Language:" +movieResult.Language,
+      "Plot:" + movieResult.Plot,
+      "Actors:" +movieResult.Actors
+    ].join("\n\n");
+
+     // Append properties and the divider to log.txt, print showData to the console
+    fs.appendFile("log.txt", listing + divider, function(err) {
+      if (err) throw err;
+      console.log(listing);
+    });
+  });
+}
+
+
+command(liriCom,input);
