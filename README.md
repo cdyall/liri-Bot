3d4a3bd248a726977f2fc901812f7b3da75502e2
cdyall <cdyall1@gmail.com>
2019-05-19 11:12:04 -0400

    commit 5

--- README.md
+++ README.md
@@ -1,1 +1,152 @@
-# liri-Bot
+# Liri
+## LIRI Bot for Week #10 Homework
+
+### About
+
+LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+### Motivation
+
+This is a project for Omar Patel's class at UCLA where we are learning to implement node.js.
+
+### What it does
+
+#### Bands in Town Artist Events API
+ `node liri.js concert-this <artist/band name here>`
+
+This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+
+#### Spotify
+`node liri.js spotify-this-song <insert song title>`
+
+This will show the following information about the song in your terminal/bash window
+
+* Artist(s)
+* The song's name
+* A preview link of the song from Spotify
+* The album that the song is from
+
+If no song is provided then your program will default to "The Sign" by Ace of Base
+
+#### Movies
+`node liri.js movie-this <insert movie title>`
+
+This will output the following information to your terminal/bash window:
+
+- Title of the movie.
+- Year the movie came out.
+- IMDB Rating of the movie.
+- Country where the movie was produced.
+- Language of the movie.
+- Plot of the movie.
+- Actors in the movie.
+- Rotten Tomatoes Rating.
+- Rotten Tomatoes URL.
+
+If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+#### Do What It Says
+`node liri.js do-what-it-says`
+
+Using the `fs` Node package, LIRI will take the text from random.txt file and then use it to call one of LIRI's commands.
+
+Right now it will run `spotify-this-song` for "I Want it That Way,".
+
+As of right now it will only work with spotitfy. I am will continue to work towards the other functions working here as well.
+
+### Authors
+
+Chris Dyall
+
+
+
+
+# liri-node-app
+
+**Creator**: `Komal Batra`
+
+**Created on**: `Oct 22nd 2018`
+
+- - -
+
+## ABOUT THE APP
+LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives back data. The user has the option of using four commands (listed below) in conjuntion with specific parameters associated with the commands. The  `Commands` are:
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
+
+- - -
+## HOW TO USE LIRI
+### **Video Guide**
+
+Watch the video here: https://drive.google.com/file/d/1rT4qbvQoZnfuvI2KZXd_XE3xHcxFMZR9/view
+
+### **Step by Step instructions**
+
+1. Open your terminal such as Bash.
+2. Navigate to the folder that contains the `liri.js` file. 
+3. Depending on the command you run, the output will vary. 
+
+    **Example 1**: Run the `concert-this` command
+    
+        node liri.js concert-this <name of artist or band>
+    
+    Output: The system will display a list of all events and locations where the artist or band will perform. It can result in multiple records. The system will also log all the results in the log.txt file. See screen-shot below:
+
+    ![Results](/screenshots/concert_this_results.PNG)
+
+    **Example 2**: Run the `spotify-this-song` command
+    
+        node liri.js spotify-this-song <name of song>
+    
+    Output: The system will display a list of information associated with the song. It can result in multiple records. The system will also log all the results in the log.txt file. See screen-shot below:
+
+    ![Results](/screenshots/spotify_this_results.PNG)
+
+    **Example 3**: Run the `movie-this` command
+    
+        node liri.js movie-this <name of movie>
+    
+    Output: The system will display information associated with the movie. The system will also log all the results in the log.txt file. See screen-shot below:
+
+    ![Results](/screenshots/movie_this_results.PNG)
+
+
+    **Example 4**: Run the `do-what-it-says` command
+        
+        node liri.js do-what-it-says
+        
+    Output: The system will read the text in the random.txt file, and perform the comman listed in the random.txt file. 
+    
+    See screen-shot below:
+
+    ![Results](/screenshots/dothis_this_results.PNG)
+
+- - -
+
+## TECHNOLOGIES USED
+* Javascript
+* Nodejs
+* Node packages:
+    * Node-Spotify-API
+    * DotEnv
+    * Axios
+    * Moment
+* APIs used:
+    * Bands in Town
+    * OMDB
+* Git
+* GitHub
--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var fs = require("fs");
+var moment = require("moment");
 
 //variables that take in the command line arguments
 var liriCom = process.argv[2];
@@ -18,9 +19,8 @@ var divider =
 
 // Here I create liri comands concert-this, spotify-this-song, movie-this and do-what-it-says.
 //  I will do this using the switch statment, this allows me to preform diffrent actions based on the conditions.
-function command(liriCom, input){
-  switch (liriCom){
-
+function command(liriCom, input) {
+  switch (liriCom) {
     case "concert-this":
       conInfo(input);
       break;
@@ -29,22 +29,23 @@ function command(liriCom, input){
       spotInfo(input);
       break;
 
-     case "movie-this":
-       movInfo(input);
-       break;
+    case "movie-this":
+      movInfo(input);
+      break;
 
-     case "do-what-it-says":
-       doThis();
-       break;
+    case "do-what-it-says":
+      doThis();
+      break;
   }
 }
 
-
-// function for "concert-this" command. 
+// function for "concert-this" command.
 function conInfo(input) {
   // query url
   var queryUrl =
-    "https://rest.bandsintown.com/artists/" +input +"/events?app_id=codingbootcamp";
+    "https://rest.bandsintown.com/artists/" +
+    input +
+    "/events?app_id=codingbootcamp";
 
   // Print the query url.
   console.log(queryUrl);
@@ -53,15 +54,15 @@ function conInfo(input) {
   axios.get(queryUrl).then(function(response) {
     var bandInfo = response.data[0];
 
-     // properties end up being the string containing the show data we will print to the console
+    // properties end up being the string containing the show data we will print to the console
     var eveInfo = [
       "Band:" + bandInfo.lineup,
       "Venue:" + bandInfo.venue.name,
       "City:" + bandInfo.venue.city,
-      "Date:" + bandInfo.datetime
+      "Date:" + moment(bandInfo.datetime).format("MM DD YYYY")
     ].join("\n\n");
 
-     // Append properties and the divider to log.txt, print showData to the console
+    // Append properties and the divider to log.txt, print showData to the console
     fs.appendFile("log.txt", eveInfo + divider, function(err) {
       if (err) throw err;
       console.log(eveInfo);
@@ -73,45 +74,46 @@ function conInfo(input) {
 function spotInfo(songName) {
   var spotify = new Spotify(keys.spotify);
 
-  //If no song is provided, use "The Sign" 
-      if (!songName) {
-          songName = "The Sign";
-      };        
+  //If no song is provided, use "The Sign"
+  if (!songName) {
+    songName = "The Sign";
+  }
 
-      console.log(songName);
+  console.log(songName);
 
-      //Callback to spotify to search for song name
-      spotify.search({ type: 'track', query: songName}, function(err, data) {
-          if (err) {
-              return console.log('Error occurred: ' + err);
-          } 
+  //Callback to spotify to search for song name
+  spotify.search({ type: "track", query: songName }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
 
-           // properties end up being the string containing the show data we will print to the console
-          var songInfo = [
-            "Artist: " + data.tracks.items[0].artists[0].name,
-            "Song name: " + data.tracks.items[0].name,
-            "Album Name: " + data.tracks.items[0].album.name,
-            "Preview Link: " + data.tracks.items[0].preview_url
-           ] .join("\n\n");
-               
-          //Appends text to log.txt file
-          fs.appendFile('log.txt', songInfo + divider, function (err) {
-              if (err) throw err;
-              console.log(songInfo);
-            });    
-      });
-};
+    // properties end up being the string containing the show data we will print to the console
+    var songInfo = [
+      "Artist: " + data.tracks.items[0].artists[0].name,
+      "Song name: " + data.tracks.items[0].name,
+      "Album Name: " + data.tracks.items[0].album.name,
+      "Preview Link: " + data.tracks.items[0].preview_url
+    ].join("\n\n");
 
-
-function movInfo(movieName) {
-
- //If no movie name is provided, use Mr.Nobody as default
- if (!movieName) {
-  movieName = "mr nobody";
+    //Appends text to log.txt file
+    fs.appendFile("log.txt", songInfo + divider, function(err) {
+      if (err) throw err;
+      console.log(songInfo);
+    });
+  });
 }
 
+function movInfo(movieName) {
+  //If no movie name is provided, use Mr.Nobody as default
+  if (!movieName) {
+    movieName = "mr nobody";
+  }
+
   // query url
-  var queryUrl = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy";
+  var queryUrl =
+    "https://www.omdbapi.com/?t=" +
+    movieName +
+    "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy";
 
   // Print the query url.
   console.log(queryUrl);
@@ -120,7 +122,7 @@ function movInfo(movieName) {
   axios.get(queryUrl).then(function(response) {
     var movieResult = response.data;
 
-     // properties end up being the string containing the show data we will print to the console
+    // properties end up being the string containing the show data we will print to the console
     var listing = [
       "Title:" + movieResult.Title,
       "Year:" + movieResult.Year,
@@ -132,7 +134,7 @@ function movInfo(movieName) {
       "Actors:" + movieResult.Actors
     ].join("\n\n");
 
-     // Append properties and the divider to log.txt, print showData to the console
+    // Append properties and the divider to log.txt, print showData to the console
     fs.appendFile("log.txt", listing + divider, function(err) {
       if (err) throw err;
       console.log(listing);
@@ -140,19 +142,31 @@ function movInfo(movieName) {
   });
 }
 
-//Function for Random
-function doThis(){
-  fs.readFile('random.txt', "utf8", function(error, data){
-    if (error) {
-      return console.log(error);
-    }
-   
-    var info = data.split(',');
-    // Sends song name back to spotify function.
-    spotInfo(info[1]);
-  });
-}
+// App functionality due to user input
+function doThis() {
+	fs.readFile('random.txt', "utf8", function(error, data){
 
+		if (error) {
+    		return console.log(error);
+  		}
 
+		// Then split it by commas (to make it more readable)
+		var dataArr = data.split(",");
 
-command(liriCom,input);
+		// I used and if statment to loop through the commands that might be present in random.txt
+		if (dataArr[0] === "spotify-this-song") {
+			var songcheck = dataArr[1].slice(1, -1);
+			spotInfo(songcheck);
+		} else if (dataArr[0] === "concert-this") {
+			var concert = dataArr[1].slice(1, -1);
+			conInfo(concert);
+		} else if(dataArr[0] === "movie-this") {
+			var listing = dataArr[1].slice(1, -1);
+			movInfo(listing);
+		} 
+		
+  	});
+
+};
+
+command(liriCom, input);
